25f67653dbab996b97604e83ff4a41a65d27158a
MangelMaxime <mangel.maxime@protonmail.com>
2021-06-30 08:15:10 +0200

    Rewrite the base-url-middleware to redirect on exact match and use temporary redirection to avoid caching from the browser

--- src/Nacara/js/base-url-middleware.js
+++ src/Nacara/js/base-url-middleware.js
@@ -1,9 +1,12 @@
 export default function (baseUrl) {
 
     return function (req, res, next) {
-        if (req.url.startsWith(baseUrl)) {
-            res.writeHead(301, {
-                Location: "http://" + req.headers.host + "/" + req.url.substring(baseUrl.length)
+        var segments = req.url.split("/").splice(1);
+        const sanitizeBaseUrl = baseUrl.replace(/\//g, "")
+        if (segments.length > 1 && segments[0] === sanitizeBaseUrl) {
+            var newUrl = segments.splice(1).join("/");
+            res.writeHead(307, {
+                Location: "http://" + req.headers.host + "/" + newUrl
             });
             res.end();
         } else {
