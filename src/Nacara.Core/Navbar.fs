133f215848049c3691071916b9b8e91cb91d855d
Maxime Mangel <mangel.maxime@protonmail.com>
2021-09-08 09:32:41 +0200

    Fix breadcrumbs paths

--- src/Nacara.Core/Navbar.fs
+++ src/Nacara.Core/Navbar.fs
@@ -8,15 +8,15 @@ let tryFindWebsiteSectionLabelForPage
         navbar.Start
         |> List.tryFind (
             function
-            | StartNavbarItem.LabelLink { Section = Some itemSection } ->
+            | StartNavbarItem.LabelLink { Section = Some itemSection }
+            | StartNavbarItem.Dropdown { Section = Some itemSection} ->
                 itemSection = pageContext.Section
 
             | _ ->
                 false
         )
         |> Option.map (function
-            | StartNavbarItem.LabelLink { Label = label } ->
+            | StartNavbarItem.LabelLink { Label = label }
+            | StartNavbarItem.Dropdown { Label = label } ->
                 label
-            | _ ->
-                failwith "Should not happen, we filtered the navbar.Start elements before"
         )
